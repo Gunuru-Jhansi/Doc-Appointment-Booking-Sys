28ae52ddd69a2b600e2ac9e50b7c12685cea3276
Jhansi Gunuru <gunurjhansi@gmail.com>
2025-07-18 10:17:20 +0530

    changes

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -4,6 +4,6 @@ const connectDB=async()=>{
 
     mongoose.connection.on("connected",()=>console.log("Database Connected"))
     
-    await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`)
+    await mongoose.connect(`${process.env.MONGODB_URI}`)
 }
 export default connectDB
